7f8364a2b6637a6e08fc301226f12f4e3cc60b66
Gaurav-7065 <jhagaurav214@gmail.com>
2026-07-08 13:18:35 +0530

    Time: 2 ms (29.02%) | Memory: 42.7 MB (46.04%) - LeetSync

--- 4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.java
+++ 4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.java
@@ -6,7 +6,7 @@ class Solution {
         }
        
         int max=(int)Math.pow(r,1.0/k)+1;
-       for(int i=0;i<=max;++i){
+       for(int i=0;i<=max;i++){
         long x=Math.round(Math.pow(i,k));
 
         if(l<=x&&x<=r) count++;
