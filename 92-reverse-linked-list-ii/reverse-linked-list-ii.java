e1784530517cfb8d1b1501f246ffbc7f9fdbd85a
Gaurav-7065 <jhagaurav214@gmail.com>
2026-07-14 12:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (29.95%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode temp = head;
@@ -18,27 +8,25 @@ class Solution {
             temp = temp.next;
             idx++;
         }
-        Stack<ListNode> st = new Stack<>();
-        while (idx != right + 1) {
-            st.push(temp);
-            temp = temp.next;
+        ListNode prev1=null;
+        ListNode curr=temp;
+        ListNode nhead=temp;
+        ListNode ahead=null;
+        while(idx!=right+1&&curr!=null){
+            
+            ahead=curr.next;
+            curr.next=prev1;
+            prev1=curr;
+            curr=ahead;
             idx++;
+            
         }
-        ListNode node = st.pop();
-        ListNode tail = node;
-        while (!st.isEmpty()) {
-            tail.next = st.pop();
-            tail = tail.next;
-        }
+        
         if (prev != null ) {
-            prev.next = node;
+            prev.next = prev1;
         }
-        if(tail!=null){
-            tail.next=temp;
-        }
-       
-
-        return left==1?node: head;
+        nhead.next=curr;
+        return left==1?prev1: head;
 
     }
 }
