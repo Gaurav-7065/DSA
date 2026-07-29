9db7fa18b1bdf8ed0da5b13ba5a722cf1e849d1f
Gaurav-7065 <jhagaurav214@gmail.com>
2026-07-29 22:04:15 +0530

    Time: 3 ms (98.45%) | Memory: 110.5 MB (90.11%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,22 +1,27 @@
 class Solution {
-    int mini=Integer.MAX_VALUE;
     public int climbStairs(int n, int[] costs) {
-        
-        int[]dp=new int[n+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0]=0;
-        for(int i=1;i<=n;i++){
-           dp[i]=Math.min(dp[i],dp[i-1]+costs[i-1]+1);
 
-           if(i>=2){
-            dp[i]=Math.min(dp[i],dp[i-2]+costs[i-1]+4);
-           }
+        if (n == 0) return 0;
 
-           if(i>=3){
-            dp[i]=Math.min(dp[i],dp[i-3]+costs[i-1]+9);
-           }
+        int dp0 = 0;                 // dp[i-3]
+        int dp1 = Integer.MIN_VALUE; // dp[i-2]
+        int dp2 = 0; // dp[i-1]
+
+        for (int i = 1; i <= n; i++) {
+
+            int curr = dp2 + costs[i - 1] + 1;
+
+            if (i >= 2)
+                curr = Math.min(curr, dp1 + costs[i - 1] + 4);
+
+            if (i >= 3)
+                curr = Math.min(curr, dp0 + costs[i - 1] + 9);
+
+            dp0 = dp1;
+            dp1 = dp2;
+            dp2 = curr;
         }
-        return dp[n];
+
+        return dp2;
     }
-     
 }
