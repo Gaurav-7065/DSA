eda7208dca7ae310e0a09d654b248b38da3298b1
Gaurav-7065 <jhagaurav214@gmail.com>
2026-08-04 21:26:49 +0530

    Time: 60 ms (40.15%) | Memory: 47.9 MB (42.75%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
@@ -1,26 +1,23 @@
 class Solution {
-    int mod = 1000000007;
-
+    int mod=1000000007;
     public int xorAfterQueries(int[] nums, int[][] queries) {
-        int n = nums.length;
+        int n=nums.length;
+        int m=queries.length;
 
-        for (int[] q : queries) {
-            int idx = q[0];
-            int ri = q[1];
-            int ki = q[2];
-            int vi = q[3];
-
-            while (idx <= ri) {
-                nums[idx] = (int)(((long) nums[idx] * vi) % mod);
-                idx += ki;
+        for(int i=0;i<m;i++){
+            int idx=queries[i][0];
+            int ri=queries[i][1];
+            int vi=queries[i][3];
+            int ki=queries[i][2];
+            while(idx<=ri){
+                nums[idx]=(int)(((long)nums[idx]*vi)%mod);
+                idx+=ki;
             }
         }
-
-        int xor = nums[0];
-        for (int i = 1; i < n; i++) {
-            xor ^= nums[i];
+        long xor=nums[0];
+        for(int i=1;i<n;i++){
+            xor=(xor^nums[i]);
         }
-
-        return xor;
+        return (int)xor;
     }
 }
