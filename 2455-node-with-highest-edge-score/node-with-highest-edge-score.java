b9ceb14747ad4463f22cb763d35ff4e3fb1fa451
Gaurav-7065 <jhagaurav214@gmail.com>
2026-08-30 12:16:59 +0530

    Time: 3 ms (100.00%) | Memory: 103.4 MB (76.23%) - LeetSync

--- 2455-node-with-highest-edge-score/node-with-highest-edge-score.java
+++ 2455-node-with-highest-edge-score/node-with-highest-edge-score.java
@@ -1,28 +1,18 @@
 class Solution {
     public int edgeScore(int[] edges) {
-        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+        int n=edges.length;
+        long []score=new long[n];
         for(int i=0;i<edges.length;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<edges.length;i++){
-            adj.get(edges[i]).add(i);
+            score[edges[i]]+=i;
         }
 
-        long ans=-1;
-        long maxScore=Integer.MIN_VALUE;
-
-        for(int i=0;i<adj.size();i++){
-            long score=0;
-            for(int sc:adj.get(i)){
-                score+=sc;
-            }
-            if(score>maxScore){
-               maxScore=score;
-               ans=i;
+        int ans=0;
+      
+        for(int i=1;i<n;i++){
+            if(score[i]>score[ans]){
+                ans=i;
             }
         }
-
-     return (int)ans;
-
+        return (int)ans;
     }
 }
